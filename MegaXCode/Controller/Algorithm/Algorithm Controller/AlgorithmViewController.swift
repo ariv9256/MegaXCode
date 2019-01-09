57bcfdff18330b89a6862ccd58927984808e6513
Angela Rivas <snowcats001@gmail.com>
2019-01-09 12:17:34 -0700

    Connected buttons on main storyboard controller to all individual projects properly (for now)

--- MegaXCode/Controller/Algorithm/Algorithm Controller/AlgorithmViewController.swift
+++ MegaXCode/Controller/Algorithm/Algorithm Controller/AlgorithmViewController.swift
@@ -37,7 +37,7 @@ class AlgorithmViewController: UIViewController
         //Creates an arrayList of the step variables
         let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven, stepEight]
         
-        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let attributesDictionary = [NSAttributedString.Key.font : algorithmText.font]
         let fullAttributedString = NSMutableAttributedString(string: title, attributes: attributesDictionary)
         
         //Adds bullet points in string steps
@@ -49,7 +49,7 @@ class AlgorithmViewController: UIViewController
             let attributedStringStep: NSMutableAttributedString = NSMutableAttributedString(string: formattedStep)
             let outlineStyle = createOutlineStyle()
             
-            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
+            attributedStringStep.addAttributes([NSAttributedString.Key.paragraphStyle : outlineStyle], range: NSMakeRange(0,attributedStringStep.length))
             
             fullAttributedString.append(attributedStringStep)
         }
